18674976b3a845ef9704eac34a7fbf08ca5bc075
werick silva <wericksilva-@hotmail.com>
2022-03-23 14:18:21 -0300

    submit entrar

--- codigo/app-loja-puc-minas/src/app/security/login/login.component.ts
+++ codigo/app-loja-puc-minas/src/app/security/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-login',
@@ -6,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  entrar() {
+    this.router.navigate(['/produtos'], {} );
   }
 
 }
